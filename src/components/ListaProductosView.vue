6fdadb2203651b4fd03314b1834c2167f126c6a2
OMARROJASP <63993140+OMARROJASP@users.noreply.github.com>
2024-07-02 08:53:20 -0500

    first commit

---
+++ src/components/ListaProductosView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1>Productos</h1>
+    <p class="text-2xl text-blue-500">{{mensaje}}</p>
+    <div v-for="user in users" :key="user.email">
+      <p>Nombre: {{ user.name }}</p>
+      <p>Email: {{ user.email }}</p>
+      <p>Edad: {{ user.age }}</p>
+      <br/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export  default {
+  name: 'ProductosView',
+  props:{
+      mensaje: String,
+    users:{
+        type:Array,
+      required: true
+    }
+  }
+}
+</script>
